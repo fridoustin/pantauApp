a86f45191b72d15cb6ba5bbdfb3dc5bd8439af96
fridoustin <fridolinaustin16@gmail.com>
2025-03-18 17:16:31 +0800

    feat: make home page

---
+++ lib/features/home/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:pantau_app/constant/colors.dart';
+
+
+class HomePage extends StatelessWidget {
+  static const String route = '/home';
+
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) { 
+    return const Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: Center(
+        child: Text(
+          "home screen",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+    );
+  }
+}
