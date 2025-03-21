c1940ac9918565e04ea1db6a9c531c97896429f0
AndiRoihan <roihanaqilah72@gmail.com>
2025-03-22 03:17:13 +0800

    feat: add signout button

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pantau_app/core/constant/colors.dart';
+import 'package:pantau_app/features/auth/presentation/providers/auth_providers.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 
-class HomePage extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   static const String route = '/home';
 
-  const HomePage({super.key});
+  const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) { 
-    return const Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) { 
+    return Scaffold(
       backgroundColor: AppColors.backgroundColor,
       body: Center(
-        child: Text(
-          "home screen",
-          style: TextStyle(
-            fontSize: 24,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "home screen",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            OutlinedButton(
+              onPressed: () async {
+                final authNotifier = ref.read(authStateProvider.notifier);
+                await authNotifier.signOut();
+                Navigator.pushReplacementNamed(context, '/login');
+              }, 
+              child: const Icon(Icons.logout)
+            )
+          ],
         ),
       ),
     );
