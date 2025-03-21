6b9228a55ab801f36d6e130ba42a9fec88100399
fridoustin <fridolinaustin16@gmail.com>
2025-03-21 16:51:32 +0800

    feat: implement login use case and repository

---
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:pantau_app/features/auth/data/datasources/auth_data_source.dart';
+import 'package:pantau_app/features/auth/domain/repositories/auth_repository.dart';
+
+class AuthRepositoryImpl implements AuthRepository {
+  final AuthDataSource dataSource;
+
+  AuthRepositoryImpl(this.dataSource);
+
+  @override
+  Future<void> signIn({required String email, required String password}) async {
+    await dataSource.signIn(email: email, password: password);
+  }
+}
---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,3 @@
+abstract class AuthRepository {
+  Future<void> signIn({required String email, required String password});
+}
