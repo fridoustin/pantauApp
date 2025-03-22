18363a73faae110973f91dc74e6872ccfcb46228
AndiRoihan <roihanaqilah72@gmail.com>
2025-03-22 18:10:00 +0800

    feat: customize error message for alert dialog

--- lib/features/auth/data/datasources/supabase_auth_data_source.dart
+++ lib/features/auth/data/datasources/supabase_auth_data_source.dart
@@ -22,10 +22,13 @@ class SupabaseAuthDataSource implements AuthDataSource {
       final userRole = response.user?.userMetadata?['role'];
       if (userRole != 'technician') {
         await client.auth.signOut();
-        throw Exception('Access denied. Only Technician can login');
+        throw 'Access denied. Only Technician can login';
       }
     } catch (e) {
-      throw Exception(e.toString());
+      if (e.toString().contains('AuthException')) {
+        throw 'The email or password entered is invalid. Please try again.';
+      }
+      throw e.toString();
     }
   }
   
