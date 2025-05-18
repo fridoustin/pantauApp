76da2fdd86cf4bf8c5f8732f94f6329cb6c757c4
AndiRoihan <roihanaqilah72@gmail.com>
2025-05-18 15:37:13 +0800

    feat: update edge function to only send notification for work order that have admin id

--- supabase/functions/push/index.ts
+++ supabase/functions/push/index.ts
@@ -23,56 +23,74 @@ const supabase = createClient(
 Deno.serve(async (req) => {
   const payload: WebhookPayload = await req.json()
 
-  const { data } = await supabase
-    .from('technician')
-    .select('fcm_token')
-    .eq('technician_id', payload.record.technician_id)
+  const { data: workorderData, error: workorderError } = await supabase
+    .from('workorder')
+    .select('admin_id')
+    .eq('id', payload.record.id)
     .single()
 
-  const fcmToken = data!.fcm_token as string
+  if (workorderData && workorderData.admin_id) {
+    const { data } = await supabase
+      .from('technician')
+      .select('fcm_token')
+      .eq('technician_id', payload.record.technician_id)
+      .single()
 
-  const accessToken = await getAccessToken({
-    clientEmail: serviceAccount.client_email,
-    privateKey: serviceAccount.private_key,
-  })
+    const fcmToken = data!.fcm_token as string
 
-  await supabase
-    .from('notification')
-    .insert({
-      'technician_id': payload.record.technician_id, 
-      'title': 'Work Order Baru', 
-      'message': `Anda menerima work order baru dengan judul ${payload.record.title}`,
-      'wo_id': payload.record.id
+    const accessToken = await getAccessToken({
+      clientEmail: serviceAccount.client_email,
+      privateKey: serviceAccount.private_key,
     })
 
-  const res = await fetch(
-    `https://fcm.googleapis.com/v1/projects/${serviceAccount.project_id}/messages:send`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${accessToken}`,
-      },
-      body: JSON.stringify({
-        message: {
-          token: fcmToken,
-          notification: {
-            title: `Work Order Baru`,
-            body: `Anda menerima work order baru dengan judul ${payload.record.title}`,
-          },
+    await supabase
+      .from('notification')
+      .insert({
+        'technician_id': payload.record.technician_id, 
+        'title': 'Work Order Baru', 
+        'message': `Anda menerima work order baru dengan judul ${payload.record.title}`,
+        'wo_id': payload.record.id
+      })
+
+    const res = await fetch(
+      `https://fcm.googleapis.com/v1/projects/${serviceAccount.project_id}/messages:send`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
         },
-      }),
+        body: JSON.stringify({
+          message: {
+            token: fcmToken,
+            notification: {
+              title: `Work Order Baru`,
+              body: `Anda menerima work order baru dengan judul ${payload.record.title}`,
+            },
+            android: {
+              priority: "High",
+            },
+          },
+        }),
+      }
+    )
+
+    const resData = await res.json()
+    if (res.status < 200 || 299 < res.status) {
+      throw resData
     }
-  )
 
-  const resData = await res.json()
-  if (res.status < 200 || 299 < res.status) {
-    throw resData
+    return new Response(JSON.stringify(resData), {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  } else {
+    return new Response(JSON.stringify({
+      message: "Work order not found or wo_id is null",
+      status: "skipped"
+    }), {
+      headers: { 'Content-Type': 'application/json' },
+    })
   }
-
-  return new Response(JSON.stringify(resData), {
-    headers: { 'Content-Type': 'application/json' },
-  })
 })
 
 const getAccessToken = ({
